4cba99b040797c4e58f628bb196620cdb1002947
actuallyfro <actuallyfro@github.com>
2023-05-07 19:43:48 -0500

    {README | LocalStorage Lib} Added README; adding first library stab

---
+++ README.md
@@ -0,0 +1,17 @@
+Simple JavaScript File IO
+=========================
+
+This quick library is for me to hack on/learn JavaScript, and enable code reuse.
+
+To Use as Submodule
+-------------------
+```
+git submodule add https://github.com/ActuallyFro/SJFI.git SJFI
+```
+
+To Commit Changes
+-----------------
+```
+git add .gitmodules new_submodule
+git commit -m "Added new submodule"
+```
---
+++ lib-localstorage.js
@@ -0,0 +1,21 @@
+// LOCALStorage
+window.saveFunction = function(storageKey, debug = true) {
+  if (debug) console.log("[SJFI] [DEBUG] saveFunction(" + storageKey + ") called");
+  localStorage.setItem(storageKey, JSON.stringify(window.graphObjects));
+};
+
+window.loadGraphObjects = function(storageKey, debug = true) {
+  if (debug) console.log("[SJFI] [DEBUG] loadGraphObjects(" + storageKey + ") called");
+  const storedGraphObjects = localStorage.getItem(storageKey);
+  if (storedGraphObjects) {
+    window.graphObjects = JSON.parse(storedGraphObjects);
+    updateTable();
+  }
+};
+
+window.resetLocalStorage = function(storageKey, debug = true) {
+  if (debug) console.log("[SJFI] [DEBUG] resetLocalStorage(" + storageKey + ") called");
+  localStorage.removeItem(storageKey);
+  window.graphObjects = [];
+  updateTable();
+};
