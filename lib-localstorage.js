0c3b63df44cc1053992991e2bb5e763a3aac8db5
actuallyfro <actuallyfro@github.com>
2023-05-20 22:35:54 -0500

    {lib-localstorage.js} Making more accurate function calls

--- lib-localstorage.js
+++ lib-localstorage.js
@@ -1,19 +1,24 @@
 // LOCALStorage
-window.saveFunction = function(storageKey, storageData, debug = false) {
+window.storeJSONObjectsIntoKey = function(storageKey, storageData, debug = false) {
   if (debug) console.log("[SJFI] [DEBUG] saveFunction(" + storageKey + ") called");
   localStorage.setItem(storageKey, JSON.stringify(storageData));
 };
 
-window.loadGraphObjects = function(storageKey, storageData, debug = false) {
-  if (debug) console.log("[SJFI] [DEBUG] loadGraphObjects(" + storageKey + ") called");
+window.loadJSONObjectsFromKey = function(storageKey, storageData, debug = false) {
+  if (debug) console.log("[SJFI] [DEBUG] loadJSONObjectsIntoKey(" + storageKey + ") called");
   const storedGraphObjects = localStorage.getItem(storageKey);
   if (storedGraphObjects) {
     storageData = JSON.parse(storedGraphObjects);
   }
 };
 
-window.resetLocalStorage = function(storageKey, storageData, debug = false) {
+window.resetLocalStorageByKey = function(storageKey, storageData, debug = false) {
   if (debug) console.log("[SJFI] [DEBUG] resetLocalStorage(" + storageKey + ") called");
   localStorage.removeItem(storageKey);
   storageData = [];
 };
+
+window.clearLocalStorageALLKeys = function(debug = false) {
+  if (debug) console.log("[SJFI] [DEBUG] clearLocalStorageALLKeys() called");
+  localStorage.clear();
+}
